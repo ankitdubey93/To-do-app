d3f02bfaec43350e3fa5ef54290e408cf2afdb8e
Ankit Dubey <ankit19933011@gmail.com>
2025-01-28 20:55:03 +0530

    todo app backend done

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 const authRouter = require("./Routes/authRoute.js");
+const todoRouter = require("./Routes/todoRoute.js");
+const authMiddleware = require("./middleware/authMiddleware.js");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const homepage_path = "index.html";
-
 // middleware
-app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(homepage_path);
+  res.status(200).send({ message: "Welcome to To-do App" });
 });
 
 app.use("/api", authRouter);
+app.use("/api/todos", authMiddleware, todoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
