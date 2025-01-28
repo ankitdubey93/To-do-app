1fc53f0348f14cc6834c82dd9b1c35be23497dd6
Ankit Dubey <ankit19933011@gmail.com>
2025-01-28 12:31:56 +0530

    frontend login and signup form done

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const homepage_path = "index.html";
+
+// middleware
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json);
+
+app.get("/", (req, res) => {
+  res.sendFile(homepage_path);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
